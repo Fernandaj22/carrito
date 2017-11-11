425b59d81d4deee8fa743e36d6baa2ed3b566520
Unknown <mjuarez22@alumnos.uaq.mx>
2017-11-10 18:06:42 -0600

    cambios 101117

---
+++ principal.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" type="text/css" href="css/estilos.css">
+<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+<style>
+@import url('https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,800i');
+</style>
+	<title>Nombre de la Tienda - Principal</title>
+</head>
+<body>
+
+	<article class="encabezado">
+			<div class="logo">
+				<i class="fa fa-shopping-bag" aria-hidden="true"> </i>
+				<h3> Nombre de la tienda</h3>
+			</div>
+			<div class="redsociales">
+				<i class="fa fa-facebook-official" aria-hidden="true"></i>
+				<i class="fa fa-twitter-square" aria-hidden="true"></i>
+				<i class="fa fa-instagram" aria-hidden="true"></i>
+			</div>
+			<div class="login">
+				<a href="">Iniciar Sesión</a>
+				<a href="">Regístrate</a>
+				<i class="fa fa-shopping-cart" aria-hidden="true"></i>
+			</div>
+	</article>
+
+	<section class="banner">
+		<div class="banner1" style="display: block !important">
+			<i class="fa fa-chevron-left der" aria-hidden="true"></i>
+			<h1>¡Bienvenido!</h1>
+			<i class="fa fa-chevron-right izq" aria-hidden="true"></i>
+			<p>No podrás parar de comprar</p>
+		</div>
+		<div class="banner2">
+			<i class="fa fa-chevron-left" aria-hidden="true"></i>
+			<h1>Promociones súper duper</h1>
+			<p>Entérate de las prendas más novedosas</p>
+			<i class="fa fa-chevron-right" aria-hidden="true"></i>
+		</div>
+		<div class="banner3">
+			<i class="fa fa-chevron-left" aria-hidden="true"></i>
+			<h1>Nuevas playeras</h1>
+			<p>Colores vivos de temporada</p>
+			<i class="fa fa-chevron-right" aria-hidden="true"></i>
+		</div>
+	</section>
+
+	<section class="articulos">
+		<div class="seccion1">
+			<a href="">PLAYERAS</a>
+		</div>
+		<div class="seccion2">
+			<a href="">CAMISAS</a>
+		</div>
+		<div class="seccion3">
+			<a href="">PANTALONES</a>
+		</div>
+		<div class="seccion4">
+			<a href="">SUÉTER Y CHAMARRAS</a>
+		</div>
+		<div class="seccion5">
+			<a href="">ZAPATOS</a>
+		</div>
+	</section>
+
+	<footer>
+		<div>
+			<p>Facultad de Informática</p>
+			<p>Centro de Desarrollo, Betas</p>
+		</div>
+
+		<div>
+			<p>Contácto</p>
+			<p>email@correo.com</p>
+		</div>
+
+		<div class="redsociales">
+				<i class="fa fa-facebook-official" aria-hidden="true"></i>
+				<i class="fa fa-twitter-square" aria-hidden="true"></i>
+				<i class="fa fa-instagram" aria-hidden="true"></i>
+		</div>
+
+	</footer>
+
+
+</body>
+</html>
