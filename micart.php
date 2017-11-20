ca0fcae6f73c21858ee033587230b5644629792f
Unknown <mjuarez22@alumnos.uaq.mx>
2017-11-19 21:19:22 -0600

    Mi carrito/Perfil

--- micart.php
+++ micart.php
@@ -28,15 +28,53 @@
 		</div>
 	</header>
 
-	<div class="container bannercart"></div>
+	<div class="container-fluid">
+		<div class="col-xs-12 imgcate0 img-responsive" id="top"></div>
+	</div>
+
+	<nav class="navbar navbar-default border" role="navigation">
+  		<div class="navbar-header txto">
+    		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+      		<span class="icon-bar"></span>
+      		<span class="icon-bar"></span>
+      		<span class="icon-bar"></span>
+    		</button> 
+    		 <a class="navbar-brand" href="" style="cursor:default;">Menú</a> 
+  		</div>
+
+  		<div class="navbar-collapse collapse container">
+	   		<ul class="nav navbar-nav navbar-left right">
+		     	 <li class="hover"><a href="#">Playeras</a></li>
+		      	 <li class="hover"><a href="camisas.php">Camisas</a></li>
+		      	 <li class="hover"><a href="pantalones.php">Pantalones</a></li>
+		      	 <li class="hover"><a href="chamarras.php">Suéter/Chamarra</a></li>
+		      	 <li class="hover"><a href="zapatos.php">Zapatos</a></li>
+	    	</ul>
+
+	    	<ul class="nav navbar-nav navbar-right home">
+        		<li class="hover"><a href="principal.php"><i class="fa fa-home" aria-hidden="true"></i> Home</a></li>
+    		</ul>
+ 		</div>
+	</nav>
 
 	<div class="container-fluid">
 		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 lado6">
-			<h2>Mi perfil</h2>
+			<h2><i class="fa fa-user" aria-hidden="true"></i> Mi perfil</h2>
 
-			<div class="personal">
-				<p>Nombre: </p> <p>María Fernanda Juárez</p>
-				<p>Correo electrónico: </p> <p>ferjuareztirado@gmail.com</p>
+			<div class="panel panel-default personal">
+			  <div class="panel-heading bold">Nombre</div>
+			  <div class="panel-body">
+			    María Fernanda Juárez Tirado
+			  </div>
+			</div>
+
+			<div class="panel panel-default">
+			  <div class="panel-heading">
+			    <h3 class="panel-title bold">Email</h3>
+			  </div>
+			  <div class="panel-body">
+			    ferjuareztirado@gmail.com
+			  </div>
 			</div>
 
 			<details>
@@ -51,16 +89,23 @@
 			  <summary>Direcciones utilizadas: </summary>
 			  <p>Av. Pie de la Cuesta 2015-A no. 117 condominio Tamayo, Querétaro, Qro.</p>
 			  <p>Av. Pie de la Cuesta 2015-A no. 117 condominio Tamayo, Querétaro, Qro.</p>
-			  <p>Av. Pie de la Cuesta 2015-A no. 117 condominio Tamayo, Querétaro, Qro.</p>0 bhhhhhhhhhhhhhhn
+			  <p>Av. Pie de la Cuesta 2015-A no. 117 condominio Tamayo, Querétaro, Qro.</p>
 			</details>
 
-			<div>
-				<h2>Historial de pedidos</h2>
+			<div class="historial">
+				<h3>Historial de pedidos</h3>
 				<div class="list-group">
 				  <div class="list-group-item">
 				    <p class="list-group-item-text">1 Pantalón Recto Azul</p>
 				    <p class="list-group-item-text">25/08/2017</p>
 				    <p class="list-group-item-text">Total de $542.32</p>
+				  </div>
+				</div>
+				<div class="list-group">
+				  <div class="list-group-item">
+				    <p class="list-group-item-text">3 Camisas a cuadros verdes</p>
+				    <p class="list-group-item-text">28/09/2017</p>
+				    <p class="list-group-item-text">Total de $1042.92</p>
 				  </div>
 				</div>
 				<div class="list-group">
@@ -72,83 +117,71 @@
 				</div>
 				<div class="list-group">
 				  <div class="list-group-item">
-				    <p class="list-group-item-text">1 Pantalón Recto Azul</p>
-				    <p class="list-group-item-text">25/08/2017</p>
-				    <p class="list-group-item-text">Total de $542.32</p>
+				    <p class="list-group-item-text">3 Camisas a cuadros verdes</p>
+				    <p class="list-group-item-text">28/09/2017</p>
+				    <p class="list-group-item-text">Total de $1042.92</p>
 				  </div>
 				</div>
 			</div>
 		</div>
 
 		<div class="col-xs-12 col-sm-6 col-md-6 col-lg-6 lado6">
-			<h2>Mi carrito</h2>
+			<h2><i class="fa fa-cart-arrow-down" aria-hidden="true"></i> Mi carrito</h2>
 
-			<h4>Tienes 10 artículos en el carrito</h4>
-			<div>
+			<h3>Tienes 10 artículos en el carrito</h3>
+			<div class="pedidos">
 				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
+				  <div class="list-group-item select pedidot">
+				    <p class="list-group-item-text bold">Camisa a cuadros roja</p>
+					<p class="list-group-item-text">Cantidad:</p> 
+					<input type="number" name="" min="0">
+					<div class="opindividual">
+						<input type="submit" name="" value="Comprar" class="colorc">
+					    <input type="submit" name="" value="Quitar" class="colore">
+					</div>
 				  </div>
 				</div>
+
 				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
+				  <div class="list-group-item select pedidot">
+				    <p class="list-group-item-text bold">Camisa a cuadros roja</p>
+					<p class="list-group-item-text">Cantidad:</p> 
+					<input type="number" name="" min="0">
+					<div class="opindividual">
+						<input type="submit" name="" value="Comprar" class="colorc">
+					    <input type="submit" name="" value="Quitar" class="colore">
+					</div>
 				  </div>
 				</div>
+
 				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
+				  <div class="list-group-item select pedidot">
+				    <p class="list-group-item-text bold">Camisa a cuadros roja</p>
+					<p class="list-group-item-text">Cantidad:</p> 
+					<input type="number" name="" min="0">
+					<div class="opindividual">
+						<input type="submit" name="" value="Comprar" class="colorc">
+					    <input type="submit" name="" value="Quitar" class="colore">
+					</div>
 				  </div>
 				</div>
+
 				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
+				  <div class="list-group-item select pedidot">
+				    <p class="list-group-item-text bold">Camisa a cuadros roja</p>
+					<p class="list-group-item-text">Cantidad:</p> 
+					<input type="number" name="" min="0">
+					<div class="opindividual">
+						<input type="submit" name="" value="Comprar" class="colorc">
+					    <input type="submit" name="" value="Quitar" class="colore">
+					</div>
 				  </div>
 				</div>
-				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
-				  </div>
-				</div>
-				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
-				  </div>
-				</div>
-				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
-				  </div>
-				</div>
-				<div class="list-group">
-				  <div class="list-group-item">
-				    <p class="list-group-item-text">Camisa a cuadros roja</p>
-				    <p class="list-group-item-text">Cantidad: </p> <input type="number" name="">
-				    <input type="submit" name="" value="Eliminar">
-				    <input type="submit" name="" value="Comprar ahora">
-				  </div>
-				</div>
+			</div>
+
+			<div class="opcompra">
+				<input type="submit" name="" value="Comprar todo" class="color0">
+				<input type="submit" name="" value="Limpiar carrito" class="color1">
 			</div>
 		</div>
 	</div>
