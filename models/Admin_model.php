709efb945937002ed0c78a9a3dff2e4a1b9c8ca0
jorggerojas <jorgelroar@gmail.com>
2017-11-29 18:23:28 -0600

    Cambio de Admin_model

--- models/Admin_model.php
+++ models/Admin_model.php
@@ -2,7 +2,7 @@
 
 	class Admin_model extends Model
 	{
-		public function agregarProductoA($Categoria, $Producto, $Descripcion, $nombre_imagen, $Precio, $Cantidad ) {
+		public function agregarProductoA($Categoria, $Producto, $Descripcion, $nombre_imagen, $Precio, $talla, $cantidad ) {
 			
 			$nombre_imagen=$_FILES['Imagen']['name'];
 			$tipo_imagen=$_FILES['Imagen']['type'];
@@ -12,12 +12,13 @@
 			if ($tipo_imagen == "image/jpeg" || $tipo_imagen == "image/jpg" || $tipo_imagen == "image/png" || $tipo_imagen == "image/gif" || $tipo_imagen == "JPG") {
 		
 				//ruta de la carpeta destino 
-				$temp = $_SERVER["DOCUMENT_ROOT"] . "/img/"; 
+				$temp = $_SERVER["DOCUMENT_ROOT"] . "img"; 
 
 				//movemos la imagen del directorio temp al nuestro 
 				move_uploaded_file($_FILES['Imagen']['tmp_name'], $temp.$nombre_imagen);
 
-				$dataA = array('idCategoria'=> $Categoria, 'nombreProducto' => $Producto, 'descripcion' => $Descripcion, 'imagen' => $nombre_imagen, 'precio' => $Precio, 'cantidad' => $Cantidad);
+				$dataA = array('idCategoria'=> $Categoria, 'nombreProducto' => $Producto, 'descripcion' => $Descripcion, 'imagen' => $nombre_imagen, 'precio' => $Precio, 'talla' => $talla,
+					'cantidad' => $cantidad);
 				//return $this->db->printInsert($dataA, 'productos');
 			}
 
