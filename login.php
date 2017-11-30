aed33ea51b2dc7acf744f72ba06a5b1133d03ead
Unknown <alemoralesb11@gmail.com>
2017-11-30 07:17:24 -0600

    Patalla pago y cambios en login

--- login.php
+++ login.php
@@ -15,16 +15,16 @@
     <!-- ===========================HEADER============================ -->
       <div class="container-fluid header">
         <div class="nombre col-md-12">
-          <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-          <h2>MenShop</h2>
+          <img src="img/nombre.png" alt="">
         </div>
       </div>
     <!-- ===========================CONTENIDO============================ -->
     <div class="container-fluid cont">
       <img src="img/regis-backgound.jpeg" id="img" class="img-decor img-responsive">
-        <div class="center col-xs-12 col-sm-7 col-md-5">
-              <button type="button" name="button" id="regis" class="change" onclick="registrarse()"><i class="fa fa-plus" aria-hidden="true"></i></button>
-              <button type="button" name="button" id="log" class="change hidden" onclick="login()"><i class="fa fa-plus" aria-hidden="true"></i></button>
+        <div class="center col-xs-10 col-sm-7 col-md-5">
+              <button type="button" name="button" id="regis" class="change" onclick="registrarse()"><img src="img/plus.png" alt=""></button>
+              <button type="button" name="button" id="log" class="change hidden" onclick="login()"><img src="img/check.png" alt=""></button>
+              <button type="button" name="button" id="regresar" class="change hidden" onclick="regresar()"><i class="fa fa-arrow-left" aria-hidden="true"></i></button>
             <!-- <div class="izq col-md-6">
               <img src="img/loginbackground.jpeg" id="img" class="img-decor"alt="">
               <img src="img/regis-backgound.jpeg" class="hidden" id="img-decor">
@@ -40,9 +40,9 @@
                       <input type="email" class=" col-xs-12  col-md-10" name=""  placeholder=" &#9993  |  Correo">
                       <input type="password" class=" col-xs-12  col-md-10" name="" placeholder="&#x1f511  |  Contraseña">
                       <input type="password" class=" col-xs-12  col-md-10" name="" placeholder="&#x1f511  |  Repite tu contraseña">
-                      <select class="pregunta  col-xs-12  col-md-10" onchange="habilitar()" name="">
-                        <option value="">Elige una opcion</option>
-                        <option value="" id="1">Nombre de tu primera mascota</option>
+                      <select class="pregunta  col-xs-12  col-md-10" onchange="habilitar();" name="">
+                        <option value="" selected>Elige una opcion</option>
+                        <option value="" id="1" >Nombre de tu primera mascota</option>
                         <option value="" id="2">Lugar donde se conocieron tus padres</option>
                         <option value="" id="3">Titulo de tu libro favorito</option>
                       </select>
@@ -53,7 +53,7 @@
                 </div>
               </form>
             </div>
-            <div class="der col-md-12 col-xs-12" id="login">
+          <div class=" der col-md-12 col-xs-12" id="login">
               <form class="ingreso" action="index.html" method="post">
                 <div class="top col-xs-12">
                     <h1>Ingresa</h1>
@@ -65,9 +65,39 @@
                 </div>
                 <div class="bottom col-xs-12">
                       <input type="submit" class="  col-xs-8 col-md-6 ingresa" name="" value="Ingresa">
-                      <h6>¿olvidaste tu contraseña?<br><a class="link" href="#">Cambiar contraseña</a></h6>
+                      <h6>¿olvidaste tu contraseña?<br></h6><input type="button" class="  link" href="#" value="Cambiar contraseña" onclick="abrirpregunta()">
                 </div>
               </form>
+          </div>
+          <div class="hidden contraseñas col-md-12 col-xs-12" id="pregunta">
+              <div class="top col-xs-12">
+                  <h1>¿Olvidaste tu contraseña?</h1>
+                  <h4>Responde esta pregunta para poder modificar tu contraseña</h4>
+              </div>
+              <div class="middle col-xs-12">
+                    <select class="pregunta  col-xs-12  col-md-10" onchange="habilitar()" name="">
+                      <option value="">Elige una opcion</option>
+                      <option value="" id="1">Nombre de tu primera mascota</option>
+                      <option value="" id="2">Lugar donde se conocieron tus padres</option>
+                      <option value="" id="3">Titulo de tu libro favorito</option>
+                    </select>
+                    <input type="text"  class=" col-xs-12  col-md-10" id="respuesta2" placeholder="&#10003; | Respuesta" disabled>
+              </div>
+              <div class="bottom col-xs-12">
+                    <input type="submit" class=" col-xs-8 col-md-6 registrate" id="pregunta-env" name="" value="Listo">
+              </div>
+          </div>
+          <div class=" hidden constraseñas col-md-12 col-xs-12" id="contraseña">
+              <div class="top col-xs-12">
+                  <h1>Cambia tu contraseña</h1>
+              </div>
+              <div class="middle col-xs-12">
+                    <input type="password" class=" col-xs-12  col-md-10" name="" placeholder="&#x1f511  |  Contraseña">
+                    <input type="password" class=" col-xs-12  col-md-10" name="" placeholder="&#x1f511  |  Repite tu contraseña">
+                  </div>
+              <div class="bottom col-xs-12">
+                    <input type="submit" class=" col-xs-8 col-md-6 registrate" id="nuevaCont" name="" value="Guardar">
+              </div>
           </div>
       </div>
     </div>
